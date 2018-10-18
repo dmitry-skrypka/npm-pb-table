b91c84b0bc7fcd0cb5e8adbd7021d45234f1f0c2
dmitry-skrypka <dimitry.skripka@gmail.com>
2018-10-19 00:33:01 +0300

    Updates

---
+++ precache-manifest.a6634a9b5711b62db1341bb0f088b844.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "38dc3157f62a1bfc6c2f",
+    "url": "/npm-pb-table/static/js/runtime~main.38dc3157.js"
+  },
+  {
+    "revision": "9448073aa8ace5fd9101",
+    "url": "/npm-pb-table/static/js/main.9448073a.chunk.js"
+  },
+  {
+    "revision": "b61b9989ab7f00b6467f",
+    "url": "/npm-pb-table/static/js/1.b61b9989.chunk.js"
+  },
+  {
+    "revision": "9448073aa8ace5fd9101",
+    "url": "/npm-pb-table/static/css/main.b7fca940.chunk.css"
+  },
+  {
+    "revision": "603ec45570d413bcb5bb8dc53a90587d",
+    "url": "/npm-pb-table/index.html"
+  }
+];
